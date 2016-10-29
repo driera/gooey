7ded470e324790868220e275faf49c8245cb3e80
Dani Riera <info@driera.com>
2016-10-29 11:28:39 +0200

    adds cursor functionality

--- src/_scripts/modules/experience.js
+++ src/_scripts/modules/experience.js
@@ -7,8 +7,15 @@ let TweenMax = require('Gsap');
 export default class Experience {
     constructor() {
         let self = this;
-        this._gooey = '.experience';
+        this._gooey = $('.experience');
         this._dots = $('.experience-gooey-dot');
+        this.$box = $('.experience-gooey-cursor');
+
+        this.enabled = false;
+        this.left = this._gooey.offset().left + (this._gooey.outerWidth() / 2);
+        this.top = this._gooey.offset().top+ (this._gooey.outerHeight() / 2);
+        this.hMax  = (this._gooey.outerWidth() - this.$box.outerWidth()) / 2.5;
+        this.vMax  = (this._gooey.outerHeight() - this.$box.outerHeight()) / 2.5;
 
         $(window).ready(function() {
             self.addEvents();
@@ -17,17 +24,18 @@ export default class Experience {
 
     addEvents() {
         let self = this;
-        $(this._gooey).on('mouseenter', function() {
+        this._gooey.on('mouseenter', function() {
             self._dots.each(function(i){
                 self.startCircleAnim($(this),20,0.1,1+(i*0.2),1.25+(i*0.7));
-                self.followCursor();
             });
+            self.enableMovement();
         });
 
-        $(this._gooey).on('mouseleave', function() {
+        this._gooey.on('mouseleave', function() {
             self._dots.each(function(i){
                 self.stopCircleAnim($(this),0.8+(i*0.1));
             });
+            self.moveBoxBack();
         });
     }
 
@@ -78,40 +86,46 @@ export default class Experience {
         });
     }
 
-    // followCursor() {
-    //     let self = this;
-    //     this.$box = $('.experience-gooey-cursor');
-    //     this.$container = $(".experience");
-    //     this.$outline = $(".experience");
-    //
-    //     this.enabled = false;
-    //     this.left = $container.offset().left;
-    //     this.max  = $container.outerWidth() - $box.outerWidth();
-    //
-    //     $box.on("mouseenter", self.enableMovement);
-    //     $container.on("mouseleave", self.moveBoxBack);
-    //
-    // }
+    enableMovement(e) {
+        let self = this;
 
-    // enableMovement() {
-    //     if (enabled) return;
-    //     enabled = true;
-    //     TweenMax.set($outline, { outlineColor: "green" });
-    //     $container.on("mousemove", moveBox);
-    // }
-    //
-    // moveBox(e) {
-    //     let self = this;
-    //     let x = e.pageX - this.left;
-    //     if (x > max) { x = max; }
-    //     TweenMax.to($box, 0.5, { x: x, rotation: 20, 		ease: Power1.easenone	 });
-    // };
-    //
-    // moveBoxBack() {
-    //     $container.off("mousemove", moveBox);
-    //     enabled = false;
-    //     TweenMax.set($outline, { outlineColor: "red" });
-    //     TweenMax.to($box, 0.75, { rotation: 0, ease: Back.easeOut });
-    // };
+        if (this.enabled) return;
+        this.enabled = true;
+        this._gooey.on("mousemove", function(e) {
+            self.moveBox(e);
+        });
+    }
+
+    moveBox(e) {
+        let self = this;
+        let y = e.pageY - this.top;
+        let x = e.pageX - this.left;
+        if (x > this.hMax) {
+            x = this.hMax;
+        }
+        if (y > this.vMax) {
+            y = this.vMax;
+        }
+        TweenMax.to(self.$box, 0.5, {
+            x: x,
+            y: y,
+            rotation: 20,
+            ease: Power1.easenone
+        });
+    };
+
+    moveBoxBack(e) {
+        let self = this;
+        this._gooey.off("mousemove", function(e) {
+            self.moveBox(e);
+        });
+        this.enabled = false;
+        TweenMax.to(self.$box, 0.75, {
+            x: 0,
+            y: 0,
+            rotation: 0,
+            ease: Back.easeOut
+        });
+    };
 
 }
