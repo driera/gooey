a8b18013f5f7d00ea2a143ed86ea09f00b8bff51
Dani Riera <info@driera.com>
2016-10-29 22:55:13 +0200

    limit dot's movement area

--- src/_scripts/modules/experience.js
+++ src/_scripts/modules/experience.js
@@ -12,8 +12,9 @@ export default class Experience {
         this.$box = $('.experience-gooey-cursor');
 
         this.enabled = false;
-        this.left = this._gooey.offset().left + (this._gooey.outerWidth() / 2);
-        this.top = this._gooey.offset().top+ (this._gooey.outerHeight() / 2);
+        this.radius = this._gooey.outerWidth() / 2;
+        this.left = this._gooey.offset().left + this.radius;
+        this.top = this._gooey.offset().top+ this.radius;
         this.hMax  = (this._gooey.outerWidth() - this.$box.outerWidth()) / 2.5;
         this.vMax  = (this._gooey.outerHeight() - this.$box.outerHeight()) / 2.5;
 
@@ -26,7 +27,7 @@ export default class Experience {
         let self = this;
         this._gooey.on('mouseenter', function() {
             self._dots.each(function(i){
-                self.startCircleAnim($(this),20,0.1,1+(i*0.2),1.25+(i*0.7));
+                self.startCircleAnim($(this),23+(i*0.1),0.1,1+(i*0.2),1.25+(i*0.7));
             });
             self.enableMovement();
         });
@@ -100,12 +101,27 @@ export default class Experience {
         let self = this;
         let y = e.pageY - this.top;
         let x = e.pageX - this.left;
-        if (x > this.hMax) {
-            x = this.hMax;
-        }
-        if (y > this.vMax) {
-            y = this.vMax;
-        }
+        // if (x > this.hMax) {
+        //     x = this.hMax;
+        // }
+        // if (x < this.hMax * -1) {
+        //     x = this.hMax * -1;
+        // }
+        // if (y > this.vMax) {
+        //     y = this.vMax;
+        // }
+        // if (y < this.vMax * -1) {
+        //     y = this.vMax * -1;
+        // }
+
+
+        console.log( x + ' ' + (this._gooey.offset().left + this.radius));
+        // if((x - this.left)^2 + (y - this.top)^2 < this.radius^2) {
+        //     console.log('inside');
+        // } else {
+        //     console.log('outside');
+        // }
+
         TweenMax.to(self.$box, 0.5, {
             x: x,
             y: y,
@@ -127,5 +143,4 @@ export default class Experience {
             ease: Back.easeOut
         });
     };
-
 }
